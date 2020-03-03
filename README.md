a12ffedb6a5687a663e7172202f608ab14325e46
dzmitry.karayedau <dzmitry.karayedau@sap.com>
2020-03-03 16:04:56 +0300

    Added logging

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cfApp
+Instruction: https://habr.com/ru/company/sap/blog/350690/
--- src/main/java/com/cf/cfApp/web/HelloWorldController.java
+++ src/main/java/com/cf/cfApp/web/HelloWorldController.java
@@ -1,19 +1,19 @@
 package com.cf.cfApp.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServlet;
 
 /**
  * @author Dzmitry Karayedau
  */
+@Slf4j
 @RestController
-public class HelloWorldServlet extends HttpServlet {
+public class HelloWorldController {
 
     @GetMapping("/hello")
     public String getProductByName() {
-
+        log.info("Hello servlet triggered");
         return "Hello";
     }
 }
